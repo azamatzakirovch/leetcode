ae49b681b3a007657561f11898e95e9c3e99b4e1
azamatzakirovch <azamateshqoziyev2@gmail.com>
2025-11-03 22:41:33 +0500

    I already solved question number 1 in c++ and python. Actually I realised that I do not have enough knowledge in c so after learning malloc operations I am going to upload the solution of first question in c language. Even more my Algorithms & Data Structure skill is not good that's why after improving my Algorithms & Data Structure skill I will solve first question in c,c++ and python language.
    © Azamat E. Zakirovch

--- python/1.py
+++ python/1.py
@@ -1,3 +1,16 @@
 class Solution:
-    def twoSum(self, nums, target: int):
-        print("Assalamu Alaykum Dunya")
+    def twoSum(self, nums, target):
+        length = len(nums)
+        start, end = 0, 0
+
+        for i in range(0, length - 1):
+            start = i
+            for j in range(i + 1, length):
+                end = j
+                if nums[start] + nums[end] == target:
+                    return [start, end]
+
+
+a = Solution()
+
+print(a.twoSum([2,7,11,15], 9))
